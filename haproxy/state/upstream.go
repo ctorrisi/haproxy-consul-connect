5de4f660a0386b75a06296ee6ac89b2c15f80f3f
Corey Torrisi <3690732+ctorrisi@users.noreply.github.com>
2025-12-23 22:05:40 +1100

    chore: add sni for tls verificaiton

--- haproxy/state/upstream.go
+++ haproxy/state/upstream.go
@@ -120,6 +120,7 @@ func generateUpstreamServers(opts Options, certStore CertificateStore, cfg consu
 		SslCertificate: crtPath,
 		SslCafile:      caPath,
 		Verify:         models.BindVerifyRequired,
+		Sni:            cfg.Name,
 		Maintenance:    models.ServerMaintenanceEnabled,
 	}
 
